1bd416e7570771c60741df3e05261ae615e8b762
tahura-sakhafia <tahur.sakhafia@gmail.com>
2023-06-21 15:11:16 +0530

    Tahu

---
+++ PillDetector-Android/app/src/main/java/APIServices.java
@@ -0,0 +1,12 @@
+import com.example.pilldetector.MainActivity;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Body;
+
+public interface APIServices {
+   // @POST("http://127.0.0.1:8000/")
+    //Call<MainActivity> postData(@Body YourRequestModel request);
+
+}
